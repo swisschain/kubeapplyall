92426fef5479b4ad4a20fbcd47648f05e2a7e828
Alexey Novichikhin <32311883+NovichikhinAlexey@users.noreply.github.com>
2020-02-14 11:41:43 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,27 @@ set -e
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
+path=$(pwd)
+for main_directory in $path/*/
+do
+    cd $main_directory
+    current_directory=${PWD##*/}
+    echo "--------------------------"
+    kubectl apply -f namespace.yaml
+    echo "--------------------------"
+done
+
+for main_directory in $path/*/
+do
+    cd $main_directory
+    current_directory=${PWD##*/}
+    echo "--------------------------"
+    kubectl apply -f configmap.yaml
+    kubectl apply -f secret.yaml
+    kubectl apply -f service.yaml
+    kubectl apply -f deployment.yaml
+    echo "--------------------------"
+done
+
 sh -c "kubectl get pods --all-namespaces"
+
