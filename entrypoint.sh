d5fbc03151654c937a3a52f67978a06fda392831
Dvin <dvyu@yandex.ru>
2020-05-14 16:38:02 +0500

    add debug info

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
+# exit when any command fails
 set -e
 
-err_report() {
-    echo "Error on line $1"
-}
-
-trap 'err_report $LINENO' ERR
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
