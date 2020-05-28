446d70095ca376f8482d1151e928c935472beb26
Dvin <dvyu@yandex.ru>
2020-05-28 16:14:00 +0500

    change the way to search namespaces

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ export KUBECONFIG=/tmp/config
 
 kubectl get nodes
 
-Namespace=$(grep -Rl "Namespace" | grep -v .git)
+Namespace=$(find / -name "namespace*")
 Configmap=$(find / -name "configmap*")
 Secret=$(find / -name secret.yaml)
 Deployment=$(find / -name deployment.yaml)
